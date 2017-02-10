c85c5e0d96d7cb07b00a334fa324e7535fb57b88
Denis <nevim@baf.xyz>
2017-02-10 11:15:10 +0100

    Verze: 4.0 (Nyni i s historii odpovedi)

--- otazky.sh
+++ otazky.sh
@@ -5,19 +5,25 @@ do
   echo "Otazka čislo" $i"."
   echo "Zadejte věk: "
   read vek
+  echo "Je vam " $vek " let."
   if [ $vek -lt 0 ]; then
-    echo "Meně než 0?!?"
+    echo "Meně než 0?!? a máte" $vek "let."
+    echo "Meně než 0?!? a máte" $vek "let." >> log
   fi
   if [ $vek -lt 15 ] && [ $vek -ge 0 ]; then
-    echo "Jste díte"
+    echo "Jste díte a máte" $vek "let."
+    echo "Jste díte a máte" $vek "let." >> log
   fi
   if [ $vek -lt 18 ] && [ $vek -ge 15 ]; then
-    echo "Jste mladivství"
+    echo "Jste mladivství a máte" $vek "let."
+    echo "Jste mladivství a máte" $vek "let." >> log
   fi
   if [ $vek -lt 120 ] && [ $vek -ge 18 ]; then
-    echo "Jste dosplělí"
+    echo "Jste dosplělí a máte" $vek "let."
+    echo "Jste dosplělí a máte" $vek "let." >> log
   fi
   if [ $vek -ge 120 ]; then
     echo "Jste Pán Času"
+    echo "Jste Pán Času" >> log
   fi
 done
